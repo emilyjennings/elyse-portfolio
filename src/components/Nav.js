48f6e0a1b71ef8cbc98ab5d9cd9ac505ef3acb01
github username <github email address>
2019-06-15 21:51:08 -0400

    moved scrolltop function

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,24 +5,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class NavBar extends Component {
 
+  scrollTop = () => {
+    $(".top").click(function(event){
+      $(window).scrollTop(0);
+    });
+
+    $(window).scroll(function(){
+      var scrollTop = $(window).scrollTop()
+      if (scrollTop > 80) {
+        $('.top').animate({
+          opacity: '1',
+        }, 1000)
+      };
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="navlinks">
-          <Link to="/bio"><div className="biolink">Bio</div></Link>
-          <Link to="/research"><div className="researchlink">Research</div></Link>
-          <Link to="/contact"><div className="contactlink">Contact</div></Link>
-          <Link to="/gallery"><div className="gallerylink">Gallery</div></Link>
-          <Link to="/"><div className="homelink">Home</div></Link>
+          <Link to="/bio" onClick={() => this.scrollTop()}><div className="biolink">Bio</div></Link>
+          <Link to="/research" onClick={() => this.scrollTop()}><div className="researchlink">Research</div></Link>
+          <Link to="/contact" onClick={() => this.scrollTop()}><div className="contactlink">Contact</div></Link>
+          <Link to="/gallery" onClick={() => this.scrollTop()}><div className="gallerylink">Gallery</div></Link>
+          <Link to="/" onClick={() => this.scrollTop()}><div className="homelink">Home</div></Link>
         </div>
 
         <button className="hamburger">&#9776;</button>
         <div className="mobile-navlinks">
-          <Link to="/bio" onClick={() => this.props.scrollTop()}><div className="biolink">Bio</div></Link>
-          <Link to="/research" onClick={() => this.props.scrollTop()}><div className="researchlink">Research</div></Link>
-          <Link to="/contact" onClick={() => this.props.scrollTop()}><div className="contactlink">Contact</div></Link>
-          <Link to="/gallery" onClick={() => this.props.scrollTop()}><div className="gallerylink">Gallery</div></Link>
-          <Link to="/" onClick={() => this.props.scrollTop()}><div className="homelink">Home</div></Link>
+          <Link to="/bio" onClick={() => this.scrollTop()}><div className="biolink">Bio</div></Link>
+          <Link to="/research" onClick={() => this.scrollTop()}><div className="researchlink">Research</div></Link>
+          <Link to="/contact" onClick={() => this.scrollTop()}><div className="contactlink">Contact</div></Link>
+          <Link to="/gallery" onClick={() => this.scrollTop()}><div className="gallerylink">Gallery</div></Link>
+          <Link to="/" onClick={() => this.scrollTop()}><div className="homelink">Home</div></Link>
           <div className="cross"><Link to=""><FontAwesomeIcon icon={['fas', 'times']} /></Link></div>
         </div>
       </div>
