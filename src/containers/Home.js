4a684eb7cbb511cfa2703d240385d82f39ea7f84
github username <github email address>
2019-06-28 21:54:17 -0400

    margins

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -40,7 +40,7 @@ scrollTop = () => {
 //cool scroll effect for the images
   parallaxBackground = () => {
     $(window).scroll(function(){
-      $(".profile-img").css("background-position", "50% "  + (($(window).scrollTop() / 20)) + "%");
+      $(".profile-img").css("background-position", "0% "  + (($(window).scrollTop() / 20) + 10) + "%");
       // $("#abroad, #college, #teaching, .story, #coding").css("background-position", "50% "  + (($(window).scrollTop() / 20)) + "%");
       // $("#jeju").css("background-position", "0% "  + (($(window).scrollTop() / 60)) + "%");
       // $("#between, #dc").css("background-position", "50% "  + (($(window).scrollTop() / 60)) + "%");
@@ -58,11 +58,7 @@ render() {
       <div className="profile-img"></div>
       <div className="bio_content">
         Elyse is a family demographer, trained in sociology, whose research focuses on understanding the causes and consequences of family events.
-      </div>
-      <div className="bio_content">
         She earned her Ph.D. in Sociology at the University of Michigan in 2013, where she was a predoctoral trainee at the <a href="https://www.psc.isr.umich.edu/">Population Studies Center</a>. She then completed a postdoctoral fellowship at the <a href="https://www.cpc.unc.edu/">Carolina Population Center</a> at the University of North Carolina.
-      </div>
-      <div className="bio_content">
         She is currently a Research Scientist at the <a href="https://www.hsph.harvard.edu/population-development/">Harvard Center for Population and Development Studies</a> at the T.H. Chan School of Public Health.
       </div>
       <div className="nepal-img">
