9179f018bfab3038c95e793c1d9971f3a9e4a57a
github username <github email address>
2019-06-30 12:25:10 -0400

    changed quotes i think

---
+++ src/containers/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import $ from 'jquery'
+
+export default class Footer extends Component {
+
+render() {
+  return (
+    <div className="footer-img">
+      <div className="quote-box">
+        <div className="quote"></div>
+        <div className="author"></div>
+      </div>
+    </div>
+
+  );
+};
+
+
+}
