115e1ebe2d1598c58d4d9c71e6c5ce9ebbdd90e1
github username <github email address>
2019-07-01 17:49:24 -0400

    some styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,6 +35,9 @@ export default class Contact extends Component {
   changeFooter = () => {
     $('.quote').text('"Research is formalized curiosity. It is poking and prying with a purpose."')
     $('.author').text('-Zora Neale Hurston')
+    $('.footer-img').css({
+      "background-image": "url('../src/components/img/attachments/Nepal 2009 504.JPG')"
+    })
   }
 
   componentDidMount(){
